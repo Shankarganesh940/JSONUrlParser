6e16d50cedc70dd6d51e69a13951f20aa236586e
Shankar Ganesh <shankarganesh940@gmail.com>
2019-06-02 13:10:25 +0530

    Initial Commit

---
+++ JSONUrlParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONUrlParser
+//
+//  Created by Greeens5 on 02/06/19.
+//  Copyright © 2019 Book. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
